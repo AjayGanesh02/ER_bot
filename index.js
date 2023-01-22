d2853927be539e7399bafd1b5bdc08790baad237
Ajay Ganesh <ajganesh2002@gmail.com>
2023-01-22 02:57:09 -0500

    removed boilerplate comments

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
-// Create a new client instance
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -10,8 +9,6 @@ const client = new Client({
         GatewayIntentBits.GuildMembers,]
 });
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
 });
