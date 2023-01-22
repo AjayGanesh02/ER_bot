303766597589717e0d67d07cb1379a38249531a5
Ajay Ganesh <ajganesh2002@gmail.com>
2023-01-22 01:53:55 -0500

    main functionality done, need to fix resending

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,]
+});
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, msg => {
+    const regex = /\b[a-z]*er\b/
+    if (regex.test(msg.content)) {
+        const word = msg.content.match(regex)[0];
+        msg.reply(`${word}? I hardly know her!`);
+    }
+})
+
+client.login(process.env.BOT_TOKEN);
+
